2ac21ffa63b36c8c1b2f710e71e61af00947a1d8
BenjaminGordonCody <b.gordoncody@gmail.com>
2023-11-19 18:56:23 +0000

    test: Checkout init

--- Tests/MockData.cs
+++ Tests/MockData.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Checkout;
 
 namespace Tests
 {
-    internal class Class1
+    internal class MockData
     {
+        public static Dictionary<string, Price> CurrentPrice()
+        {
+            var priceList = new Dictionary<string, Price>();
+            priceList["A"] = new Price(50, 3, 130);
+            priceList["B"] = new Price(30, 2, 45);
+            priceList["C"] = new Price(20, null, null);
+            priceList["D"] = new Price(15, null, null);
+            return priceList;
+        }
+
+        public static Dictionary<string, Price> EmptyPrice => new Dictionary<string, Price>();
+
+        public static Dictionary<string, Price> PriceWithMultiplierButNoSpecialPrice
+        {
+            get
+            {
+                var priceList = new Dictionary<string, Price>();
+                priceList["A"] = new Price(50, 3, null);
+                return priceList;
+            }
+        }
+
+        public static Dictionary<string, Price> PriceWithSpecialPriceButNoMultiplier
+        {
+            get
+            {
+                var priceList = new Dictionary<string, Price>();
+                priceList["A"] = new Price(50, null, 4);
+                return priceList;
+            }
+        }
     }
+
+
 }
--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -1,11 +1,36 @@
+using Checkout;
+
 namespace Tests
 {
-    public class UnitTest1
+    
+    public class UnitTests
     {
         [Fact]
-        public void Test1()
+        public void Checkout_Init_ExpectedInputThrowsNoErrors()
         {
+            var exception = Record.Exception(() => new Checkout.Checkout(MockData.CurrentPrice()));
+            Assert.Null(exception);
+        }
 
+        [Fact]
+        public void Checkout_Init_EmptyPriceInputThrowsErrors()
+        {
+            var exception = Record.Exception(() => new Checkout.Checkout(MockData.EmptyPrice));
+            Assert.NotNull(exception);
+        }
+
+        [Fact]
+        public void Checkout_Init_PriceInputWithMultiplierButNoSpecialPriceThrowsErrors()
+        {
+            var exception = Record.Exception(() => new Checkout.Checkout(MockData.PriceWithMultiplierButNoSpecialPrice));
+            Assert.NotNull(exception);
+        }
+
+        [Fact]
+        public void Checkout_Init_PrinceInputWithSpecialPriceButNoMultiplier()
+        {
+            var exception = Record.Exception(() => new Checkout.Checkout(MockData.PriceWithSpecialPriceButNoMultiplier));
+            Assert.NotNull(exception);
         }
     }
 }
